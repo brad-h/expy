0f4c716ddcce14cb6516401173dc4701957952a5
brad-h <brad.hawbaker@gmail.com>
2017-09-26 20:43:10 -0500

    pylint

--- ExPy/ExPy/module8.py
+++ ExPy/ExPy/module8.py
@@ -6,6 +6,10 @@ import math
 NUMBER_OF_SLICES = 8
 
 def ex8():
+    """ Prompt the user for number of people and number of pizzas
+    Print the number of slices each person will get and the number
+    of leftover pieces
+    """
     number_of_people = int(input('How many people? '))
     number_of_pizzas = int(input('How many pizzas do you have? '))
 
@@ -13,7 +17,7 @@ def ex8():
     pieces_per_person = total_slices // number_of_people
     leftover_pieces = total_slices % number_of_people
     if pieces_per_person == 1:
-        piece = 'piece' 
+        piece = 'piece'
     else:
         piece = 'pieces'
 
@@ -22,11 +26,14 @@ def ex8():
     print('There are {} leftover pieces'.format(leftover_pieces))
 
 def ex8c():
+    """ Prompt the user for the number of people and how many slices for each person
+    Print the number of pizzas that should be ordered
+    """
     number_of_people = int(input('How many people? '))
     pieces_per_person = int(input('How many pieces for each person? '))
 
     total_slices = number_of_people * pieces_per_person
     number_of_pizzas = math.ceil(total_slices / NUMBER_OF_SLICES)
-        
+
     print('{} people with {} pizzas'.format(number_of_people, number_of_pizzas))
     print('There are {} pizzas'.format(number_of_pizzas))
