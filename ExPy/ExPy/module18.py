bd290feccb6d8526036cdb1d77f80b64c3830dd1
Brad Hawbaker <brad.hawbaker@gmail.com>
2017-12-18 13:23:27 -0600

    gui version

--- ExPy/ExPy/module18.py
+++ ExPy/ExPy/module18.py
@@ -1,5 +1,8 @@
 """ Temperature Converter """
 
+import rx
+import tkcomponents
+
 def fahrenheit_to_celsius(fahrenheit):
     """ Given Fahrenheit, convert to Celsius """
     celsius = (fahrenheit - 32.) * (5. / 9.)
@@ -39,6 +42,30 @@ def ex18():
 
     print('The temperature in {} is {}'.format(to_scale, converted))
 
+def ex18gui():
+    """ GUI version of the temperature conversion program"""
+    root = tkcomponents.create('Temperature Conversion')
+    options = [(value, text) for (value, (_, text, _)) in SCALES.items()]
+    scales = tkcomponents.radio_stream(root, options, 0, default='C')
+    temperature_label = (scales
+                         .map(lambda x: SCALES[x][0])
+                         .map('Please enter the temperature in {}'.format))
+    temperatures = tkcomponents.input_stream(root, temperature_label, 1)
+
+    def callback(scale, temperature):
+        """ Callback to calcluate the converted temperature """
+        try:
+            temp = float(temperature)
+        except ValueError:
+            return 'Enter a valid number'
+        converted = SCALES[scale][2](temp)
+        to_scale = SCALES[scale][0]
+        return 'Temperature in {} is {}'.format(to_scale, converted)
+
+    #pylint: disable=E1101
+    outputs = rx.Observable.combine_latest(scales, temperatures, callback)
+    tkcomponents.output_label(root, outputs, 2)
+    root.mainloop()
 
 if __name__ == '__main__':
-    ex18()
+    ex18gui()
