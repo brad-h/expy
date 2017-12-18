94cf12c0f7735c3ef5144bd70f89425d17951197
Brad Hawbaker <brad.hawbaker@gmail.com>
2017-12-18 14:04:29 -0600

    ex 19

---
+++ ExPy/ExPy/module19.py
@@ -0,0 +1,42 @@
+""" BMI Calculator """
+
+def calculate_bmi(weight, height):
+    """ Given weight (pounds), height (inches)
+    Return BMI
+    """
+    return (weight / (height * height)) * 703.
+
+def bmi_recommendation(bmi):
+    """Given a BMI, return a recommendation"""
+    if bmi < 18.5:
+        return 'You are underweight. You should see a doctor.'
+    elif bmi < 25:
+        return 'You are within the ideal weight range.'
+    else:
+        return 'You are overweight. You should see a doctor.'
+
+def prompt_float(prompt):
+    """ Given a specified prompt, return a float """
+
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print('Enter a valid number')
+
+def ex19():
+    """ Prompt for weight and height
+    Print BMI and BMI range
+    """
+
+    weight = prompt_float('Enter weight in pounds(lbs): ')
+    height = prompt_float('Enter height in inches: ')
+
+    bmi = calculate_bmi(weight, height)
+    recommendation = bmi_recommendation(bmi)
+
+    print('Your BMI is {}'.format(bmi))
+    print(recommendation)
+
+if __name__ == '__main__':
+    ex19()
