e2cb16c1fa1e6fe9bb69f5a79edfb4d4be52b63f
Brad Hawbaker <brad.hawbaker@gmail.com>
2017-12-18 22:10:26 -0600

    ex25 gui

--- ExPy/ExPy/module25.py
+++ ExPy/ExPy/module25.py
@@ -1,5 +1,8 @@
 """ Password Strength Indicator """
 
+import rx
+import tkcomponents
+
 VERY_WEAK = 0
 WEAK = 1
 STRONG = 2
@@ -19,8 +22,7 @@ def password_validator(password):
         if any(map(str.isalpha, password)) and any(map(str.isdigit, password)):
             if any(map(lambda x: not str.isalnum(x) and str.isprintable(x), password)):
                 return VERY_STRONG
-            else:
-                return STRONG
+            return STRONG
 
 PASSWORD_STRENGTH_DESCRIPTION = {
     VERY_WEAK: 'very weak',
@@ -33,7 +35,9 @@ def password_strength_description(password_strength):
     """Given a password strength: VERY_STRONG, WEAK, STRONG or VERY_STRONG
     Return a string to be displayed to the user
     """
-    return PASSWORD_STRENGTH_DESCRIPTION[password_strength]
+    if password_strength in PASSWORD_STRENGTH_DESCRIPTION:
+        return PASSWORD_STRENGTH_DESCRIPTION[password_strength]
+    return 'unknown strength'
 
 def ex25():
     """ Prompt for a password
@@ -41,11 +45,21 @@ def ex25():
     """
     password = input('Password: ')
     strength_code = password_validator(password)
-    if strength_code is None:
-        strength = 'unknown strength'
-    else:
-        strength = password_strength_description(strength_code)
+    strength = password_strength_description(strength_code)
     print("The password '{}' is a {} password".format(password, strength))
 
+def ex25gui():
+    """ GUI version of ex 25 """
+    root = tkcomponents.create('Password Strength Validator')
+    #pylint: disable=E1101
+    passwords = tkcomponents.input_stream(root, rx.Observable.just('Password'), 0)
+    def callback(password):
+        """Given a password, return the output"""
+        strength_code = password_validator(password)
+        strength = password_strength_description(strength_code)
+        return "The password '{}' is a {} password".format(password, strength)
+    tkcomponents.output_label(root, passwords.map(callback), 1)
+    root.mainloop()
+
 if __name__ == '__main__':
-    ex25()
+    ex25gui()
