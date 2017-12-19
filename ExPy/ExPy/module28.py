17881d015770d880d87f9a6d7cf50c9f45438ea4
Brad Hawbaker <brad.hawbaker@gmail.com>
2017-12-19 12:38:32 -0600

    pylint

--- ExPy/ExPy/module28.py
+++ ExPy/ExPy/module28.py
@@ -3,17 +3,17 @@
 def ex28():
     """Prompt user for numbers 5 times and show the sum"""
     counter = int(input('Enter how many numbers to sum: '))
-    sum = 0
+    accumulator = 0
     loop = True
     while loop:
         try:
-            sum += int(input('Enter a number: '))
+            accumulator += int(input('Enter a number: '))
         except ValueError:
             print('Enter valid numbers')
         else:
             counter -= 1
         loop = counter > 0
-    print('The total is {}.'.format(sum))
+    print('The total is {}.'.format(accumulator))
 
 if __name__ == '__main__':
     ex28()
