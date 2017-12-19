64c482254ef6d4812f409852d5af8969763779ba
Brad Hawbaker <brad.hawbaker@gmail.com>
2017-12-18 21:27:17 -0600

    ex24

---
+++ ExPy/ExPy/module24.py
@@ -0,0 +1,33 @@
+""" Anagram Checker """
+
+def is_anagram(first, second):
+    """ Checks to see if both input strings are anagrams of each eother
+    first - (str) first string
+    second - (str) second string
+    Returns (bool) - True if x is an anagram of y, otherwise False
+    """
+    assert isinstance(first, str)
+    assert isinstance(second, str)
+    firsts = list(first)
+    firsts.sort()
+    seconds = list(second)
+    seconds.sort()
+    return ''.join(firsts) == ''.join(seconds)
+
+def ex24():
+    """ Prompt for two strings
+    Check if they are anagrams
+    """
+
+    first = input('Enter the first string: ')
+    second = input('Enter the second string: ')
+
+    anagram = is_anagram(first, second)
+
+    if anagram:
+        print('{} is an anagram of {}'.format(first, second))
+    else:
+        print('{} is not an anagram of {}'.format(first, second))
+
+if __name__ == '__main__':
+    ex24()
