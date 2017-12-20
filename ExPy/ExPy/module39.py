ad24d2b3fe375451b9ce67288d9f371c0281dcf2
Brad Hawbaker <brad.hawbaker@gmail.com>
2017-12-20 14:09:19 -0600

    ex 39 with helper module and data set

---
+++ ExPy/ExPy/module39.py
@@ -0,0 +1,27 @@
+"""Sorting Records"""
+
+import csv
+from prettyprinttable import pretty_print_table
+
+ORDER = dict(zip('LPS', 'Last Name,Position,Separation Date'.split(',')))
+
+def ex39():
+    """Read the data set, sort and display it"""
+    records = []
+    with open('module39.csv') as filehandle:
+        reader = csv.DictReader(filehandle)
+        for row in reader:
+            records.append(row)
+    options = 'Sort by ' + ', '.join('{} ({})'.format(v, k) for k, v in ORDER.items()) + ': '
+    while True:
+        order = input(options).upper()
+        if order in ORDER:
+            order = ORDER[order]
+            break
+        print('Select a valid option')
+    records.sort(key=lambda x: x[order])
+    print(pretty_print_table(records, records[0].keys()))
+
+
+if __name__ == '__main__':
+    ex39()
---
+++ ExPy/ExPy/prettyprinttable.py
@@ -0,0 +1,30 @@
+"""Tabular helper functions"""
+
+import os
+
+def pretty_print_table(records, columns):
+    """Given an iterable of dicts with key and value both of type str
+    And an iterable of strings that are keys in those dict
+    Return a Pretty-print table as str
+    """
+    column_max = {}
+
+    header = []
+    seperator = []
+    for column in columns:
+        col_len = len(column)
+        maximum = max(len(r[column]) for r in records if column in r and r[column] is not None)
+        maximum = maximum if maximum > col_len else col_len
+        column_max[column] = maximum
+        header.append(column.ljust(maximum))
+        seperator.append('-' * maximum)
+
+    rows = [' | '.join(header), '-|-'.join(seperator)]
+
+    for record in records:
+        row = []
+        for column in columns:
+            field = record[column] if column in record and record[column] is not None else ''
+            row.append(field.ljust(column_max[column]))
+        rows.append(' | '.join(row))
+    return os.linesep.join(rows)
